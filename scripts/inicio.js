7d13655c064552e398b72ee4dcab573ae8cbb7eb
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2024-11-12 00:47:04 -0300

    contador e neve

--- scripts/inicio.js
+++ scripts/inicio.js
@@ -1,7 +1,16 @@
-let contador = document.getElementById("contador");
-let contDias = document.getElementById("contDias");
-let teste = new Date();
+const dataAtual = new Date();
+const newyeartime = new Date(dataAtual.getTime() + (new Date('2024-12-25T00:00:00').getTime() - dataAtual.getTime()));
 
-contDias.innerHTML = `${teste.getDate()}`
+const atualizarContador = () => {
+    let currentTime = new Date()
+    let difference = newyeartime - currentTime;
+    let dias = Math.floor(difference / 1000 / 60 / 60 / 24);
+    let horas = Math.floor(difference / 1000 / 60 / 60) % 24;
+    let minutos = Math.floor(difference / 1000 / 60) % 60;
+    let segundos = Math.floor(difference / 1000) % 60;
+    
+    contDias.innerHTML = `${dias}`;
+    contador.innerHTML = `${horas}:${minutos}:${segundos}`;
+}
 
-contador.innerHTML = `${teste.getHours()}:${teste.getMinutes()}:${teste.getSeconds()}`
+setInterval(atualizarContador, 1000);
